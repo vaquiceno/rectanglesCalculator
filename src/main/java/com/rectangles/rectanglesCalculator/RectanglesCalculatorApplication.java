111be194d0d47203c76eac5cff454fcdcf983572
vaquiceno <Cla3215>
2022-04-19 23:44:16 -0500

    Initial commit

---
+++ src/main/java/com/rectangles/rectanglesCalculator/RectanglesCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.rectangles.rectanglesCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RectanglesCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RectanglesCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rectangles/rectanglesCalculator/RectanglesCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rectangles.rectanglesCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RectanglesCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
